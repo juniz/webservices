aa4162c9165e361c7c3ed3b42ba06b4ef2b3b1df
Yudo <53389941+juniz@users.noreply.github.com>
2023-02-27 14:26:09 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://webapps.rsbhayangkaranganjuk.com'],
 
     'allowed_origins_patterns' => [],
 
