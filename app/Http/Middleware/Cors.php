34f62daf51b7305eedf4c42475f50cc12f2d75ef
Yudo <53389941+juniz@users.noreply.github.com>
2023-03-06 08:39:04 +0700

    comit

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+            ->header('Access-Control-Allow-Methods', 'GET, POST')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Access-Control-Allow-Origin, Accept');
     }
 }
